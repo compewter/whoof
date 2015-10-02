cc0a4b3ee1d470e7a622ca3da5079d99c8617afd
Michael Wetherald <michaelwetherald@gmail.com>
2015-10-02 14:07:30 -0700

    restructured to have admin and client listening on separate ports

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 var express = require('express');
-var router = require('./router');
 
-var app = express();
-
-
-app.use( express.static(__dirname + '/../client/') );
+var clientApp = express();
+clientApp.use( express.static(__dirname + '/../client/clientApp') );
+clientApp.listen( process.env.PORT || 8080 );
 
 
-app.listen( process.env.PORT || 1337 );
+//It seems odd to me that we have our admin pages available from the internet. 
+//The admin app will be available on a different port.
+var adminApp = express();
+adminApp.use( express.static(__dirname + '/../client/adminApp/') );
+adminApp.listen( 1337 );
 
-module.exports = app;
+
+module.exports.clientApp = clientApp;
+module.exports.adminApp  = adminApp;
