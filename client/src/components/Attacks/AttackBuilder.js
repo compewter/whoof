db474b450ec63fa271022cc5985c41136b80e4dd
Michael Wetherald <michaelwetherald@gmail.com>
2017-02-18 13:03:20 -0800

    Cleaned up attack builder

--- client/src/components/Attacks/AttackBuilder.js
+++ client/src/components/Attacks/AttackBuilder.js
@@ -4,11 +4,44 @@ import 'brace/mode/java';
 import 'brace/theme/monokai';
 
 class AttackBuilder extends Component {
-  componentDidMount() {
-    this._prepare = `function prepare(params, logger){\n  logger("Preparing custom attack...");\n  return params;\n}`
-    this._execute = `function execute(params){\n  params.time = new Date()\n  socket.emit("result", {\n    success: true,\n    message: "Successfully executed attack",\n    params\n  });\n}`
-    this._followup = `function followup(params, logger){\n  logger("Successfully pwnd victim at " + params.time);\n}`
+  componentWillMount() {
+    this._functionDefaults = [
+      {
+        name: 'prepare',
+        description: [
+          'This function is used to programatically prepare any data for the execute function.',
+          'Add any data you need to the params object.',
+          'User inputs are attached to the params argument.',
+          'The logger argument is provided for printing information.',
+          'You must return the params object.'
+        ],
+        defaultValue: `function prepare(params, logger){\n  logger("Preparing custom attack...");\n  return params;\n}`
+      },
+      {
+        name: 'execute',
+        description: [
+          'This function is executed in the victim\'s browser.',
+          'The params argument is configured in the prepare function.',
+          'If you do not emit a result with the victim\'s socket the attacker\'s console will not know the status of the executed function- e.g.,',
+          'socket.emit("result", {',
+          '  success: true',
+          '  message: "Successfully executed attack",',
+          '  params: params',
+          '});'
+        ],
+        defaultValue: `function execute(params){\n  params.time = new Date();\n  socket.emit("result", {\n    success: true,\n    message: "Successfully executed attack",\n    params\n  });\n}`
+      },
+      {
+        name: 'followup',
+        description: [
+          'This function is executed on the attackers side when the victim\'s socket emits a result.',
+          'The params argument is the one returned by the attack execute function.'
+        ],
+        defaultValue: `function followup(params, logger){\n  logger("Successfully pwnd victim at " + params.time);\n}`
+      }
+    ]
   }
+
   _buildAttack = () => {
     this.props.logger('Building custom attack...')
     let attack = {
@@ -21,19 +54,6 @@ class AttackBuilder extends Component {
     this.props.execute(attack)
   }
 
-  _setPrepare = (newValue)=>{
-    console.log(newValue)
-    this._prepare = newValue
-  }
-
-  _setExecute = (newValue)=>{
-    this._execute = newValue
-  }
-
-  _setFollowup = (newValue)=>{
-    this._followup = newValue
-  }
-
   render(){
     return(
       <tbody>
@@ -44,81 +64,36 @@ class AttackBuilder extends Component {
           <td>Attack Builder</td>
           <td>Build a custom attack</td>
         </tr>
-        <tr className={this.props.active ? 'content active' : 'content'}>
-          <td>
-            <h5>Prepare:</h5>
-            <br></br>
-            <p>
-              This function is used to programatically prepare any data for the attack function.<br></br>
-              Add any data you need to the params object.<br></br>
-              The values of user inputs are attached to the params argument.<br></br>
-              The logger argument is provided for printing information.<br></br>
-              You must return the params object.
-            </p>
-          </td>
-          <td>
-            <AceEditor
-              mode="javascript"
-              theme="monokai"
-              name="attack-prep-ta"
-              height='300px'
-              tabSize={2}
-              onChange={this._setPrepare}
-              editorProps={{$blockScrolling: true}}
-              value={this._prepare}
-            />
-          </td>
-        </tr>
-        <tr className={this.props.active ? 'content active' : 'content'}>
-          <td>
-            <h5>Execute:</h5>
-            <br></br>
-            <p>
-              This function is executed in the victim's browser.<br></br>
-              The params argument is configured in the prepare function.<br></br>
-              If you do not emit a result with the victim's socket the attacker's console will not know the status of the executed function- e.g.,<br></br>
-              socket.emit("result", {'{'}<br></br>
-                &nbsp;&nbsp;success: true<br></br>
-                &nbsp;&nbsp;message: "Successfully executed attack",<br></br>
-                &nbsp;&nbsp;params: params<br></br>
-              });
-            </p>
-          </td>
-          <td>
-            <AceEditor
-              mode="javascript"
-              theme="monokai"
-              name="attack-execute-ta"
-              height='300px'
-              onChange={this._setExecute}
-              tabSize={2}
-              editorProps={{$blockScrolling: true}}
-              value={this._execute}
-            />
-          </td>
-        </tr>
-        <tr className={this.props.active ? 'content active' : 'content'}>
-          <td>
-            <h5>Followup:</h5>
-            <br></br>
-            <p>
-              This function is executed on the attackers side when the victim's socket emit's a result.<br></br>
-              The params argument is the one returned by the attack execute function.<br></br>
-            </p>
-          </td>
-          <td>
-            <AceEditor
-              mode="javascript"
-              theme="monokai"
-              name="attack-execute-ta"
-              height='300px'
-              onChange={this._setFollowup}
-              tabSize={2}
-              editorProps={{$blockScrolling: true}}
-              value={this._followup}
-            />
-          </td>
-        </tr>
+
+        {this._functionDefaults.map((options)=>{
+          this[`_${options.name}`] = options.defaultValue
+          return (<tr className={this.props.active ? 'content active' : 'content'} key={`ab_${options.name}`}>
+            <td>
+              <h5 className="description-header">{options.name}:</h5>
+              <br></br>
+              <p>
+                {options.description.map((line, idx)=>{
+                  return <span key={`${options.name}_desc_${idx}`}>{line}<br></br></span>
+                })}
+              </p>
+            </td>
+            <td>
+              <AceEditor
+                mode="javascript"
+                theme="monokai"
+                name="attack-prep-ta"
+                height='300px'
+                tabSize={2}
+                onChange={(newValue)=>{
+                  this[`_${options.name}`] = newValue
+                }}
+                editorProps={{$blockScrolling: true}}
+                value={this[`_${options.name}`]}
+              />
+            </td>
+          </tr>)
+        })}
+
         <tr className={this.props.active ? 'content active' : 'content'}>
           <td colSpan="2">
             <button 
