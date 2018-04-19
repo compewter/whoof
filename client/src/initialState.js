2d9822c736dbaa0eb1fde0bbae8083e3cd5359a9
Michael Wetherald <michael@thecompewterlab.com>
2018-04-18 17:44:15 -0700

    Update default functions in attack builder

--- client/src/initialState.js
+++ client/src/initialState.js
@@ -24,17 +24,17 @@ export default {
       'prepare': {
         name: 'prepare',
         description: 'This function is used to programatically prepare any data for the execute function.\nAdd any data you need to the params object.\nUser inputs are automatically attached to the params argument.\nThe logger argument is provided for printing information.\nYou must return the params object.',
-        function: 'function prepare(params, logger){\n  logger("Preparing custom attack...");\n  return params;\n}'
+        function: 'function prepare(params, logger){\n  //associate any necessary data to params\n  return params;\n}'
       },
       'execute': {
         name: 'execute',
         description: 'This function is executed in the victim\'s browser.\nThe params argument is configured in the prepare function.\nIf you do not emit a result with the victim\'s socket the attacker\'s console will not know the status of the executed function- e.g.,\nsocket.emit("result", {\n  success: true\n  message: "Successfully executed attack",\n  params: params\n});',
-        function: 'function execute(params){\n  params.time = new Date();\n  socket.emit("result", {\n    success: true,\n    message: "Successfully executed attack",\n    params\n  });\n}'
+        function: 'function execute(params){\n  try{\n    params.time = new Date();\n    socket.emit("result", {\n      success: true,\n      message: "Successfully executed attack",\n      params\n    });\n  }catch(e){\n    socket.emit("result", {\n      success: false,\n      message: "Attack failed: "+e,\n      params\n    });\n  }\n}'
       },
       'followup': {
         name: 'followup',
         description: 'This function is executed on the attackers side when the victim\'s socket emits a result.\nThe params argument is the one returned by the attack execute function.',
-        function: 'function followup(params, logger){\n  logger("Successfully pwnd victim at " + params.time);\n}'
+        function: 'function followup(params, logger){\n  //Follow up with another attack?\n}'
       },
       pending: {
         name: 'new',
