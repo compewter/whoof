0e984e7655916b472f405ae13a89f492d2197e39
Michael Wetherald <michael@thecompewterlab.com>
2018-06-28 13:08:21 -0700

    Reorder functions in routes/admin/sockets. Add log for successfully saved attacks

--- routes/admin/sockets.js
+++ routes/admin/sockets.js
@@ -24,87 +24,6 @@ module.exports.configure = function (io) {
   })
 }
 
-function saveAttack(attack){
-  Attack.save(Attack.translateForDB(attack)).then(()=>{
-    getAttacks()
-  })
-  .catch((err)=>{
-    module.exports.emit('message', 'Unable to save new attack. ' + err.errors[0].message)
-    console.log(err)
-  })
-}
-
-function deleteAttack(attackId){
-  Attack.deleteById(attackId).then(()=>{
-    getAttacks()
-  })
-  .catch((err)=>{
-    console.log(err)
-  })
-}
-
-function login(socket, sessID){
-  socket.join('admins')
-
-  socket.on('attackVictim', attackVictim)
-  socket.on('disconnect', disconnect)
-  socket.on('getAttacks', getAttacks)
-  socket.on('getUsers', getUsers)
-  socket.on('saveAttack', saveAttack)
-  socket.on('deleteAttack', deleteAttack)
-  socket.emit('authorized', sessID)
-}
-
-function testLogin(password, socket){
-  if(password === process.env.ADMIN_APP_PASSWD){
-    let sessID = uuidv4()
-    authorizedAdminSessionIDS[sessID] = true
-    login(socket, sessID)
-  }else{
-    socket.emit('login-required')
-  }
-}
-
-function getUsers() {
-  Object.values(victimSockets.victimsBySessionId).forEach((victim)=>{
-    module.exports.emit('newVictim', victim)
-  })
-}
-
-function getAttacks() {
-  Attack.findAll((attacks)=>{
-    module.exports.emit('attacks', assembleAttacks(attacks))
-  })
-}
-
-function disconnect() {
-  console.log('admin disconnected')
-}
-
-function attackVictim(data) {
-  console.log("received instructions to use attack " + data.attack + " on " + data.victimSocket)
-  server.ioVictim.to(data.victimSocket).emit('execute', {
-    func: `var attack = ${data.attack.toString()}`,
-    params: data.params
-  })
-}
-
-function findCookieVal(cookieStr='', cookieName){
-  let val = ''
-  cookieStr.split(';').forEach((cookie)=>{
-    let [k,v] = cookie.split('=')
-    if(k === cookieName){
-      val = v
-    }
-  })
-  return val
-}
-
-module.exports.emit = function(){
-  //need to define the emit function to prevent errors when a victim connects before an admin
-  console.log('No admins connected')
-}
-
 function assembleAttacks(attacks){
   return attacks.map((attack)=>{
     let assembledAttack = ['prepare','execute','followup'].reduce((pv, type)=>{
@@ -128,4 +47,86 @@ function assembleAttacks(attacks){
       ...assembledAttack
     }
   })
+}
+
+function attackVictim(data) {
+  console.log("received instructions to use attack " + data.attack + " on " + data.victimSocket)
+  server.ioVictim.to(data.victimSocket).emit('execute', {
+    func: `var attack = ${data.attack.toString()}`,
+    params: data.params
+  })
+}
+
+function deleteAttack(attackId){
+  Attack.deleteById(attackId).then(()=>{
+    getAttacks()
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+}
+
+function disconnect() {
+  console.log('admin disconnected')
+}
+
+module.exports.emit = function(){
+  //need to define the emit function to prevent errors when a victim connects before an admin
+  console.log('No admins connected')
+}
+
+function findCookieVal(cookieStr='', cookieName){
+  let val = ''
+  cookieStr.split(';').forEach((cookie)=>{
+    let [k,v] = cookie.split('=')
+    if(k === cookieName){
+      val = v
+    }
+  })
+  return val
+}
+
+function getUsers() {
+  Object.values(victimSockets.victimsBySessionId).forEach((victim)=>{
+    module.exports.emit('newVictim', victim)
+  })
+}
+
+function getAttacks() {
+  Attack.findAll((attacks)=>{
+    module.exports.emit('attacks', assembleAttacks(attacks))
+  })
+}
+
+function login(socket, sessID){
+  socket.join('admins')
+
+  socket.on('attackVictim', attackVictim)
+  socket.on('disconnect', disconnect)
+  socket.on('getAttacks', getAttacks)
+  socket.on('getUsers', getUsers)
+  socket.on('saveAttack', saveAttack)
+  socket.on('deleteAttack', deleteAttack)
+  socket.emit('authorized', sessID)
+}
+
+function saveAttack(attack){
+  Attack.save(Attack.translateForDB(attack)).then(()=>{
+    module.exports.emit('message', 'New attack saved.')
+    getAttacks()
+  })
+  .catch((err)=>{
+    module.exports.emit('message', 'Unable to save new attack. ' + err.errors[0].message)
+    console.log(err)
+  })
+}
+
+function testLogin(password, socket){
+  if(password === process.env.ADMIN_APP_PASSWD){
+    let sessID = uuidv4()
+    authorizedAdminSessionIDS[sessID] = true
+    login(socket, sessID)
+  }else{
+    socket.emit('login-required')
+  }
 }
